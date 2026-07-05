6674d9446b760414f0bcf7ae36c044aa813c93d1
muhamedmujanovicb <muhamed.mujanovic.b@gmail.com>
2026-07-05 21:55:28 +0200

    second commit

--- RentACarPortal/Program.cs
+++ RentACarPortal/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Fleet}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
