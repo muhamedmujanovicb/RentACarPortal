12af5e7f1d4752656e84054acf7e0471b5ea6024
muhamedmujanovicb <muhamed.mujanovic.b@gmail.com>
2026-07-12 15:00:33 +0200

    commit 8

--- RentACarPortal/Controllers/DashboardController.cs
+++ RentACarPortal/Controllers/DashboardController.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RentACarPortal.Data;
 using RentACarPortal.Models;
+using SQLitePCL;
 
 namespace RentACarPortal.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public DashboardController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Dashboard(string loggedInUser)
         {
@@ -18,6 +28,25 @@ namespace RentACarPortal.Controllers
         {
             ViewBag.Username = loggedInUser;
             return View("FleetManager");
+        }
+
+        [HttpGet]
+        public IActionResult FleetOverview(string loggedInUser)
+        {
+            Console.WriteLine($"DEBUG: Looking for user: '{loggedInUser}'");
+
+            var user = _context.Users.Include(u => u.Vehicles).FirstOrDefault(u => u.Username == loggedInUser);
+            if (user == null)
+            {
+                Console.WriteLine("DEBUG: User not found!");
+            }
+            else
+            {
+                Console.WriteLine($"DEBUG: Found user {user.Username} with {user.Vehicles.Count} vehicles");
+            }
+
+            ViewBag.Username = loggedInUser;
+            return View(user?.Vehicles.ToList()??new List<Vehicle>());
         }
     }
 }
---
+++ RentACarPortal/Controllers/FleetOverviewController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RentACarPortal.Data;
+using RentACarPortal.Models;
+using SQLitePCL;
+
+namespace RentACarPortal.Controllers
+{
+
+    public class FleetOverviewController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public FleetOverviewController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult FleetOverview()
+        {
+            return View();
+        }
+    }
+
+
+}
