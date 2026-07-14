34c54434c0b99cf831d9ba105526688e5d7b8825
muhamedmujanovicb <muhamed.mujanovic.b@gmail.com>
2026-07-14 18:02:16 +0200

    Commit 9 -Fixed bool values not displaying properly in FleetOverview -Fixed bool values not saving properly in FleetManager

--- RentACarPortal/Controllers/FleetManagerController.cs
+++ RentACarPortal/Controllers/FleetManagerController.cs
@@ -21,18 +21,16 @@ namespace RentACarPortal.Controllers
         }
 
         [HttpPost]
-        public IActionResult NewVehicle(Vehicle newVehicle, string loggedInUser)
+        public IActionResult NewVehicle([Bind("Make,Model,Year,NumberOfSeats,IsDiesel,FuelEfficiency,FuelTankSize,TypeOfVehicle,HasChildrenSafety,DriveTerrain,BootSpace,ACtype,HasNavigation,DailyRate,HasInsurance,RegisterNumberOfVehicle")] Vehicle newVehicle, string loggedInUser)
         {
             var user = _context.Users.FirstOrDefault(u => u.Username == loggedInUser);
-
             if (user != null)
             {
                 newVehicle.UserId = user.Id;
                 _context.Vehicles.Add(newVehicle);
                 _context.SaveChanges();
             }
-
-            return RedirectToAction("Dashboard", "Dashboard");
+            return RedirectToAction("Dashboard", "Dashboard", new { loggedInUser = loggedInUser });
         }
     }
 }
