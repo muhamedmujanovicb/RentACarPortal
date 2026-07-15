a6184e915d26de881fb761d046324591a11f95c9
muhamedmujanovicb <muhamed.mujanovic.b@gmail.com>
2026-07-16 00:00:04 +0200

    Commit 10 -Added a delete option for vehicles in FleetOverview -Redesigned the features section in vehicle cards in FleetOverview

--- RentACarPortal/Controllers/FleetOverviewController.cs
+++ RentACarPortal/Controllers/FleetOverviewController.cs
@@ -21,6 +21,19 @@ namespace RentACarPortal.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult DeleteVehicle(int id, string loggedInUser)
+        {
+            var vehicle = _context.Vehicles.FirstOrDefault(v => v.Id == id);
+            if (vehicle != null)
+            {
+                _context.Vehicles.Remove(vehicle);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Dashboard", "Dashboard", new { loggedInUser = loggedInUser });
+        }
     }
 
 
