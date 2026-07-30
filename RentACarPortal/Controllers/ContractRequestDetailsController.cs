74eadc5148507aeebc7a7f7bb54dd18d65a6194b
muhamedmujanovicb <muhamed.mujanovic.b@gmail.com>
2026-07-30 13:56:48 +0200

    Commit 16 - Added ContractRequestDetails view for reviewing incoming contract request details in the admin user dashboard - Added ContractRequestDetailsController for the above mentioned view

---
+++ RentACarPortal/Controllers/ContractRequestDetailsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace RentACarPortal.Controllers
+{
+    public class ContractRequestDetailsController : Controller
+    {
+        [HttpGet]
+        public IActionResult ContractRequestDetails()
+        {
+            return View();
+        }
+    }
+}
